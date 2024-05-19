b2e77f02d66f2767c63d72f3896b0acc1cd23e4f
sudipstha08 <sudipstha08@gmail.com>
2024-05-19 14:39:03 +0545

    feat: add js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,16 @@
-console.log("Welcome to My Gulp Project");
+const input = document.querySelector("input")
+const signupBtn = document.querySelector("button")
+let inputValue = ""
+
+input.addEventListener("input", (e) => {
+  console.log(e)
+  input.value = e.target.value
+  inputValue = e.target.value
+})
+
+console.log("si", input)
+
+signupBtn.addEventListener('click', () => {
+  console.log("fsdfsd")
+  window.alert(`You successfully signed up for newsletter. Your email is ${inputValue}`)
+})
